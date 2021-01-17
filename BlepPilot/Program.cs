a2234d74e189b78d549132da464c5fe11e150e3f
gws <nuez.jr@gmail.com>
2021-01-16 23:52:43 -0500

    init

---
+++ BlepPilot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BlepPilot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
